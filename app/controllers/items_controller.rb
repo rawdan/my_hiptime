5d88ba5f3869ee30a5951a03606d79afe4f68283
Andrei Crudu <bogdancrudu@yahoo.com>
2016-06-29 20:27:19 +0300

    Add index view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -8,5 +13,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description)
+  end
+
 end
