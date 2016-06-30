f2b40db447bacef8661a0cc17f2a8a039bd721bb
Andrei Crudu <bogdancrudu@yahoo.com>
2016-07-01 02:01:57 +0300

    fix sign_out GET error

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items
   root 'items#index'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
